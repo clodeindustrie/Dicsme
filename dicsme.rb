637cd6d1f9c60d92443300422ee338243ff4826a
clodeindustrie <clodeindustrie@gmail.com>
2011-10-30 23:05:19 +0000

    New start+bundler

--- dicsme.rb
+++ dicsme.rb
@@ -1,147 +1,142 @@
 # dicsme.rb
-  require 'rubygems'
-  require 'sinatra'
-  require 'dm-core'
-  require 'dm-validations'
-  require 'dm-migrations/adapters/dm-sqlite-adapter'
-  require "dm-migrations"
-  require "dm-serializer"  
-   
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-validations'
+require 'dm-migrations/adapters/dm-sqlite-adapter'
+require "dm-migrations"
+require "dm-serializer"
+
+#
+### CONFIGURATION
+
+configure :development do
+  DataMapper.setup(:default, 'sqlite3:db/dicsme.db')
+  set :logging, true
+  set :raise_errors, true
+  DataMapper::Logger.new(STDOUT, :debug)
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+
+  set :raise_errors, true
+end
+
+#
+# #
+# ### MODELS
+# #
+#
+
+class LP
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :UID,       String,  :required => true, :key => true, :unique => true
+  property :identifier, String,  :required => true, :key => true, :unique => true
+
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+end
+
+class Player
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :playing, String
+    property :updated_at, DateTime
+end
+
+DataMapper.finalize
+
+#
+# # Actions for the Daemon side
+#
+
+# Apparition du disc
+get '/app/:uid' do
+  lp = LP.first( :UID => params[:uid])
+
+  if !lp.nil?
+    play(lp.id)
+  end
+end
+
+# Disparition du disc
+get '/dis/:uid' do
+  lp = LP.first( :UID => params[:uid])
+
+  if !lp.nil?
+    stop(id)
+  end
+end
+
+get '/' do
+  "<h1>Dicsme</h1><p>/playing - currenty playing song</p><p>/list - list of tracks in db</p><p>/track/:id - track infos</p><p>POST /add - add track [:uid,:id]</p><p>delete /:id - delete track</p>"
+end
+
+#
+# # Actions for XBMC
+#
+get '/playing' do
+  LP.all( :UID => Player.first.playing ).to_json( :only => [:UID, :identifier])
+end
+
+get '/list' do
+  LP.all.to_json( :only => [:UID, :identifier])
+end
+
+get '/track/:id' do
+  LP.all( :identifier => params[:id] ).to_json( :only => [:UID, :identifier])
+end
+
+post '/add' do
+  lp = LP.new(
+    :UID        => params[:uid],
+    :identifier => params[:id],
+    :created_at => Time.now,
+    :updated_at => Time.now
+  )
+
+  if lp.save
+    "[{ 'success' : 'Album saved' }]"
+  else
+    lp.errors
+  end
+end
+
+delete '/:id' do
+  lp = LP.first( :identifier => params[:id] )
+
+  if !lp.nil?
+    lp.destroy
+    "[{ 'success' : 'Album deleted' }]"
+  else
+    "[{ 'error' : 'Paire Doesn\'t exist' }]"
+  end
+end
 
   #
-  ### CONFIGURATION
-  
-  configure :development do
-    DataMapper.setup(:default, 'sqlite3:db/dicsme.db')
-    set :logging, true
-    set :raise_errors, true
-    DataMapper::Logger.new(STDOUT, :debug)
-  end
-  
-  configure :production do
-    DataMapper.setup(:default, ENV['DATABASE_URL'])
-    
-    set :raise_errors, true
-  end
-  
-    # 
-    # #
-    # ### MODELS
-    # #  
-    #
-    
-    class LP
-      include DataMapper::Resource
+  # # misc functions
+  #
 
-      property :id,         Serial
-      property :UID,       String,  :required => true, :key => true, :unique => true
-      property :identifier, String,  :required => true, :key => true, :unique => true
-      
-      property :created_at, DateTime
-      property :updated_at, DateTime
-      
-    end
-    
-	class Player
-      include DataMapper::Resource
-	
-	  property :id, Serial
-	  property :playing, String
-      property :updated_at, DateTime
-	end
+def play(id)
+  pl = Player.first
+  pl.playing = id
+  pl.updated_at = Time.now
+  pl.save
+end
 
+def stop(id)
+  pl = Player.first
+  pl.playing = ""
+  pl.updated_at = Time.now
+  pl.save
+end
 
-    DataMapper.finalize
-    
-    
-    #
-    # # Actions for the Daemon side
-    #
-    
-    # Apparition du disc
-    get '/app/:uid' do
-		lp = LP.first( :UID => params[:uid])
-	 
-		if !lp.nil?
-		 	play(lp.id)
-		end
-    end
-    
-	# Disparition du disc
-    get '/dis/:uid' do
-    	lp = LP.first( :UID => params[:uid])
+def toAdd(uid)
 
-   		if !lp.nil?
-			stop(id)
-		end
-    end
-    
-    
-    get '/' do
-      "Default page"
-    end
-    
-    #
-    # # Actions for XBMC
-    #
-    get '/playing' do
-      LP.all( :UID => Player.first.playing ).to_json( :only => [:UUID, :identifier])
-    end
-    
-    get '/list' do
-      LP.all.to_json( :only => [:UUID, :identifier])
-    end
-    
-    get '/track/:id' do
-      LP.all( :identifier => params[:id] ).to_json( :only => [:UUID, :identifier])
-    end
-    
-    post '/add' do
-      lp = LP.new(
-        :UUID       => params[:uid],
-        :identifier => params[:id],
-        :created_at => Time.now,
-        :updated_at => Time.now
-      )
-      
-      if lp.save
-        "[{ 'success' : 'Album saved' }]"
-      else
-        lp.errors
-      end
-    end
-    
-    delete '/:id' do
-      lp = LP.first( :identifier => params[:id] )
-      
-      if !lp.nil?
-        lp.destroy
-        "[{ 'success' : 'Album deleted' }]"
-      else
-        "[{ 'error' : 'Paire Doesn\'t exist' }]"
-      end      
-    end
-    
-    
-    #
-    # # misc functions
-    #
-    
-    def play(id)
-      pl = Player.first
-	  pl.playing = id
-	  pl.updated_at = Time.now
-	  pl.save
-    end
-    
-    def stop(id)
-      pl = Player.first
-	  pl.playing = ""
-	  pl.updated_at = Time.now
-	  pl.save
-    end
-    
-    def toAdd(uid)
-      
-    end
-      
+end
+
